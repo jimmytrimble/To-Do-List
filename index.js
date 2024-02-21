4433f065f7da625ba704c2230abf0ff8d68554fb
MarthTheKing <marththeking1501@gmail.com>
2024-02-21 09:06:11 -0700

    added "add task" functionality

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let addButton = document.getElementById("add")
+addButton.addEventListener("click", function(){
+        if(document.getElementById("input").value){
+        let list = document.getElementById("todo")
+        let newItem = document.createElement("li")
+        newItem.innerHTML = document.getElementById("input").value
+        list.appendChild(newItem)
+        document.getElementById("input").value = ""
+    }
+})
+
 console.log("My code is running");
