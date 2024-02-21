acd1ec1dbb9a766ab3a5d72187a39b3dd50bcc1f
MarthTheKing <marththeking1501@gmail.com>
2024-02-21 10:09:49 -0700

    added entry removal

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 let addButton = document.getElementById("add")
 
 function addStrikethrough(event){
-    element = event.target
+    element = event.target.parentNode
     element.innerHTML = `<s>${element.innerHTML}</s>`
     element.removeEventListener("click",addStrikethrough)
+}
+
+function deleteEntry(event){
+    event.target.parentNode.remove()
 }
 
 addButton.addEventListener("click", function(){
     if(document.getElementById("input").value){
         let list = document.getElementById("todo")
         let newItem = document.createElement("li")
-        newItem.innerHTML = document.getElementById("input").value
+        let text = document.createTextNode(" " + document.getElementById("input").value)
+        let deleteButton = document.createElement("button")
+        deleteButton.innerHTML = "Remove"
+        newItem.appendChild(deleteButton)
+        newItem.appendChild(text)
         list.appendChild(newItem)
-        newItem.addEventListener("click",addStrikethrough)
+        deleteButton.addEventListener("click",deleteEntry)
         document.getElementById("input").value = ""
     }
 })
