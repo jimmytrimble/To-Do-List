800721948f1943df8debed391adcd8f1d5eea141
MarthTheKing <marththeking1501@gmail.com>
2024-02-21 09:26:47 -0700

    added input clear functionality, testing strikethrough functionality

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 let addButton = document.getElementById("add")
+
+let count = 0;
+
+/*/function addStrikethrough(id){
+    element = document.getElementById(id)
+    element.innerHTML = `<s>${element.innerHTML}</s>`
+}/*/
+
 addButton.addEventListener("click", function(){
-        if(document.getElementById("input").value){
+    if(document.getElementById("input").value){
+        count++
         let list = document.getElementById("todo")
         let newItem = document.createElement("li")
         newItem.innerHTML = document.getElementById("input").value
+        newItem.setAttribute("id",count.toString())
         list.appendChild(newItem)
         document.getElementById("input").value = ""
     }
